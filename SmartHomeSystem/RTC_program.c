461929b886e56e268c8a67de565b216c3359684f
MoEsaam <107277051+MoEsaam@users.noreply.github.com>
2023-09-15 17:23:29 +0300

    Update RTC_program.c

--- SmartHomeSystem/RTC_program.c
+++ SmartHomeSystem/RTC_program.c
@@ -3,6 +3,7 @@
 #include "TWI_interface.h"
 #include "RTC_interface.h"
 
+/* INITIALIZE RTC MODULE */
 void RTC_voidInitialization(void)
 {
 	/* INITIALIZE TWI MODULE AS A MASTER */
@@ -11,56 +12,106 @@ void RTC_voidInitialization(void)
     /* TRANSMIT START COMMUNICATION */
     TWI_SendStartCondition();
 
-    /* CONNECT TO RTC MODULE BY SENDING IT'S ID THE BUS */
-    TWI_SendSlaveAddressWithWrite(RTC_WriteMode);        // Connect to DS1307 by sending its ID on I2c Bus
-    TWI_MasterWriteDataByte(RTC_ControlRegAddress);// Select the Ds1307 ControlRegister to configure Ds1307
+    /* CONNECT TO RTC MODULE (WRITE MODE) BY SENDING IT'S ID ON THE BUS */
+    TWI_SendSlaveAddressWithWrite(RTC_WriteMode);
 
-    TWI_MasterWriteDataByte(0x00);                        // Write 0x00 to Control register to disable SQW-Out
+    /* SELECT RTC CONTROL REGISTER */
+    TWI_MasterWriteDataByte(RTC_ControlRegAddress);
 
-	TWI_SendStopCondition();                             // Stop I2C communication after initializing DS1307
+    /* WRITE 0x00 TO CONTROL REGISTER TO DISABLE SQW-Out */
+    TWI_MasterWriteDataByte(0x00);
+
+    /* TRANSMIT STOP COMMUNICATION */
+	TWI_SendStopCondition();
 }
-void RTC_SetDateTime(E_RTC *rtc)
-{
-	TWI_SendStartCondition();                          // Start I2C communication
 
-    TWI_SendSlaveAddressWithWrite(RTC_WriteMode);      // connect to DS1307 by sending its ID on I2c Bus
-    TWI_MasterWriteDataByte(RTC_SecondRegAddress); // Request sec RAM address at 00H
-    TWI_MasterWriteDataByte(((rtc->RTC_u8Second/10)<<4) + (rtc->RTC_u8Second%10));                    // Write sec from RAM address 00H
-    TWI_MasterWriteDataByte(((rtc->RTC_u8Minute/10)<<4) + (rtc->RTC_u8Minute%10));                    // Write min from RAM address 01H
-    TWI_MasterWriteDataByte(((rtc->RTC_u8Hour/10)<<4) + (rtc->RTC_u8Hour%10));              // Write hour from RAM address 02H
-    TWI_MasterWriteDataByte(((rtc->RTC_u8Weekday/10)<<4) + (rtc->RTC_u8Weekday%10));                // Write weekDay on RAM address 03H
-    TWI_MasterWriteDataByte(((rtc->RTC_u8Date/10)<<4) + (rtc->RTC_u8Date%10)) ;                   // Write date on RAM address 04H
-    TWI_MasterWriteDataByte(((rtc->RTC_u8Month/10)<<4) + (rtc->RTC_u8Month%10));                    // Write month on RAM address 05H
-    TWI_MasterWriteDataByte(((rtc->RTC_u8Year/10)<<4) + (rtc->RTC_u8Year%10));             // Write year on RAM address 06h
+/* SET DATE AND TIME OF RTC MODULE */
+void RTC_SetDateTime(E_RTC *rtc){
 
-    TWI_SendStopCondition();                              // Stop I2C communication after Setting the Date
+	/* TRANSMIT START COMMUNICATION */
+	TWI_SendStartCondition();
+
+	/* CONNECT TO RTC MODULE (WRITE MODE) BY SENDING IT'S ID ON THE BUS */
+    TWI_SendSlaveAddressWithWrite(RTC_WriteMode);
+
+    /* SELECT RTC SECOND ADDRESS REGISTER */
+    TWI_MasterWriteDataByte(RTC_SecondRegAddress);
+
+    /* WRITE SECOND IN RAM ADDRESS 00H */
+    TWI_MasterWriteDataByte(((rtc->RTC_u8Second / 10) << 4) + (rtc->RTC_u8Second % 10));
+
+    /* WRITE MINUTE IN RAM ADDRESS 01H */
+    TWI_MasterWriteDataByte(((rtc->RTC_u8Minute / 10) << 4) + (rtc->RTC_u8Minute % 10));
+
+    /* WRITE HOUR IN RAM ADDRESS 02H */
+    TWI_MasterWriteDataByte(((rtc->RTC_u8Hour / 10) << 4) + (rtc->RTC_u8Hour % 10));
+
+    /* WRITE WEEKDAY IN RAM ADDRESS 03H */
+    TWI_MasterWriteDataByte(((rtc->RTC_u8Weekday / 10) << 4) + (rtc->RTC_u8Weekday % 10));
+
+    /* WRITE DATE IN RAM ADDRESS 04H */
+    TWI_MasterWriteDataByte(((rtc->RTC_u8Date / 10) << 4) + (rtc->RTC_u8Date % 10));
+
+    /* WRITE MONTH IN RAM ADDRESS 05H */
+    TWI_MasterWriteDataByte(((rtc->RTC_u8Month / 10) << 4) + (rtc->RTC_u8Month % 10));
+
+    /* WRITE YEAR IN RAM ADDRESS 06H */
+    TWI_MasterWriteDataByte(((rtc->RTC_u8Year / 10) << 4) + (rtc->RTC_u8Year % 10));
+
+    /* TRANSMIT STOP COMMUNICATION */
+    TWI_SendStopCondition();
 }
-void RTC_GetDateTime(E_RTC *rtc)
-{
-	TWI_SendStartCondition();                            // Start I2C communication
 
-	TWI_SendSlaveAddressWithWrite(RTC_WriteMode);        // connect to DS1307 by sending its ID on I2c Bus
-	TWI_MasterWriteDataByte(RTC_SecondRegAddress); // Request Sec RAM address at 00H
+/* GET DATE AND TIME OF RTC MODULE */
+void RTC_GetDateTime(E_RTC *rtc){
 
-	TWI_SendStopCondition();                                // Stop I2C communication after selecting Sec Register
+	/* TRANSMIT START COMMUNICATION */
+	TWI_SendStartCondition();
 
-	TWI_SendStartCondition();                            // Start I2C communication
-	TWI_SendSlaveAddressWithRead(RTC_WriteMode);            // connect to DS1307(Read mode) by sending its ID
+	/* CONNECT TO RTC MODULE (WRITE MODE) BY SENDING IT'S ID ON THE BUS */
+	TWI_SendSlaveAddressWithWrite(RTC_WriteMode);
 
-    rtc->RTC_u8Second = TWI_MasterReadDataByte_ACK();// read second and return Positive ACK
-    rtc->RTC_u8Minute = TWI_MasterReadDataByte_ACK();                 // read minute and return Positive ACK
-    rtc->RTC_u8Hour= TWI_MasterReadDataByte_ACK();               // read hour and return Negative/No ACK
-    rtc->RTC_u8Weekday = TWI_MasterReadDataByte_ACK();           // read weekDay and return Positive ACK
-    rtc->RTC_u8Date= TWI_MasterReadDataByte_ACK();              // read Date and return Positive ACK
-    rtc->RTC_u8Month=TWI_MasterReadDataByte_ACK();            // read Month and return Positive ACK
-    rtc->RTC_u8Year =TWI_MasterReadDataByte_NACK();             // read Year and return Negative/No ACK
-    TWI_SendStopCondition();         // Stop I2C communication after reading the Date
-    rtc->RTC_u8Second=((rtc->RTC_u8Second& 0xF0) >> 4)*10 + (rtc->RTC_u8Second& 0x0F);
-    rtc->RTC_u8Minute=((rtc->RTC_u8Minute& 0xF0) >> 4)*10 + (rtc->RTC_u8Minute& 0x0F);
-    rtc->RTC_u8Hour=((rtc->RTC_u8Hour& 0xF0) >> 4)*10 + (rtc->RTC_u8Hour& 0x0F);
-    rtc->RTC_u8Weekday=((rtc->RTC_u8Weekday& 0xF0) >> 4)*10 + (rtc->RTC_u8Weekday& 0x0F);
-    rtc->RTC_u8Date=((rtc->RTC_u8Date& 0xF0) >> 4)*10 + (rtc->RTC_u8Date& 0x0F);
-    rtc->RTC_u8Month=((rtc->RTC_u8Month& 0xF0) >> 4)*10 + (rtc->RTC_u8Month& 0x0F);
-    rtc->RTC_u8Year=((rtc->RTC_u8Year& 0xF0) >> 4)*10 + (rtc->RTC_u8Year& 0x0F);
+	/* SELECT RTC SECOND ADDRESS REGISTER */
+	TWI_MasterWriteDataByte(RTC_SecondRegAddress);
 
+	/* TRANSMIT STOP COMMUNICATION */
+	TWI_SendStopCondition();
+
+	/* TRANSMIT START COMMUNICATION */
+	TWI_SendStartCondition();
+
+	/* CONNECT TO RTC MODULE (READ MODE) BY SENDING IT'S ID ON THE BUS */
+	TWI_SendSlaveAddressWithRead(RTC_WriteMode);
+
+	/* READ SECOND AND RETURN ACKNOWLEDGE */
+    rtc->RTC_u8Second = TWI_MasterReadDataByte_ACK();
+
+    /* READ MINUTE AND RETURN ACKNOWLEDGE */
+    rtc->RTC_u8Minute = TWI_MasterReadDataByte_ACK();
+
+    /* READ HOUR AND RETURN NO ACKNOWLEDGE */
+    rtc->RTC_u8Hour= TWI_MasterReadDataByte_ACK();
+
+    /* READ WEEKDAY AND RETURN ACKNOWLEDGE */
+    rtc->RTC_u8Weekday = TWI_MasterReadDataByte_ACK();
+
+    /* READ DATE AND RETURN ACKNOWLEDGE */
+    rtc->RTC_u8Date= TWI_MasterReadDataByte_ACK();
+
+    /* READ MONTH AND RETURN ACKNOWLEDGE */
+    rtc->RTC_u8Month=TWI_MasterReadDataByte_ACK();
+
+    /* READ YEAR AND RETURN NO ACKNOWLEDGE */
+    rtc->RTC_u8Year =TWI_MasterReadDataByte_NACK();
+
+    /* TRANSMIT STOP COMMUNICATION */
+    TWI_SendStopCondition();
+
+    rtc->RTC_u8Second  = ((rtc->RTC_u8Second & 0xF0) >> 4) * 10 + (rtc->RTC_u8Second & 0x0F);
+    rtc->RTC_u8Minute  = ((rtc->RTC_u8Minute & 0xF0) >> 4) * 10 + (rtc->RTC_u8Minute & 0x0F);
+    rtc->RTC_u8Hour    = ((rtc->RTC_u8Hour & 0xF0) >> 4) * 10 + (rtc->RTC_u8Hour & 0x0F);
+    rtc->RTC_u8Weekday = ((rtc->RTC_u8Weekday & 0xF0) >> 4) * 10 + (rtc->RTC_u8Weekday & 0x0F);
+    rtc->RTC_u8Date    = ((rtc->RTC_u8Date & 0xF0) >> 4) * 10 + (rtc->RTC_u8Date & 0x0F);
+    rtc->RTC_u8Month   = ((rtc->RTC_u8Month & 0xF0) >> 4) * 10 + (rtc->RTC_u8Month & 0x0F);
+    rtc->RTC_u8Year    = ((rtc->RTC_u8Year & 0xF0) >> 4) * 10 + (rtc->RTC_u8Year & 0x0F);
 }
